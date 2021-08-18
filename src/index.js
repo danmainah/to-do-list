04e7e8ed738a54c318b36a417745e800369c8c22
unknown <danielmainawaweru@gmail.com>
2021-08-18 13:04:09 +0300

    Fix Webhint errors

--- src/index.js
+++ src/index.js
@@ -5,35 +5,34 @@ const tasks = [
   {
     index: 1,
     isCompleted: false,
-    description: "do coding challenge",
+    description: 'do coding challenge',
   },
   {
     index: 2,
     isCompleted: false,
-    description: "Read the Bible",
+    description: 'Read the Bible',
   },
   {
     index: 3,
     isCompleted: false,
-    description: "Read emails",
+    description: 'Read emails',
   },
 ];
 
-function displaylist(){
-  tasks.forEach(task =>{
+function displaylist() {
+  tasks.forEach(task => {
     const li = document.createElement('li')
-    const text = `<div> <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    const text = `<div> <input class='form-check-input me-1' type='checkbox' value='' aria-label='...'>
     ${task.description}</div>`
-    li.classList.add("list-group-item");
+    li.classList.add('list-group-item')
     li.innerHTML = text
     ul.appendChild(li)
-  
   })
   const deleteButton = document.createElement('button')
     deleteButton.innerText = 'Clear all Completed'
-    deleteButton.classList.add("list-group-item")
-    deleteButton.classList.add("block")
+    deleteButton.classList.add('list-group-item')
+    deleteButton.classList.add('block')
     ul.appendChild(deleteButton)
-}
+};
 
 displaylist();
