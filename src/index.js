a43d6775f6d5c9db42097a14974ca84deace8b33
unknown <danielmainawaweru@gmail.com>
2021-08-18 13:18:23 +0300

    Fix Eslint errors

--- src/index.js
+++ src/index.js
@@ -20,19 +20,19 @@ const tasks = [
 ];
 
 function displaylist() {
-  tasks.forEach(task => {
-    const li = document.createElement('li')
+  tasks.forEach((task) => {
+    const li = document.createElement('li');
     const text = `<div> <input class='form-check-input me-1' type='checkbox' value='' aria-label='...'>
-    ${task.description}</div>`
-    li.classList.add('list-group-item')
-    li.innerHTML = text
-    ul.appendChild(li)
+    ${task.description}</div>`;
+    li.classList.add('list-group-item');
+    li.innerHTML = text;
+    ul.appendChild(li);
   })
-  const deleteButton = document.createElement('button')
-    deleteButton.innerText = 'Clear all Completed'
-    deleteButton.classList.add('list-group-item')
-    deleteButton.classList.add('block')
-    ul.appendChild(deleteButton)
-};
+  const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Clear all Completed';
+    deleteButton.classList.add('list-group-item');
+    deleteButton.classList.add('block');
+    ul.appendChild(deleteButton);
+}
 
 displaylist();
