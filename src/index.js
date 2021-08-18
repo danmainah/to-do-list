039b319d5b3e74803b38897614fd5cc40b88d4d8
unknown <danielmainawaweru@gmail.com>
2021-08-18 12:49:28 +0300

    Add styling

--- src/index.js
+++ src/index.js
@@ -22,10 +22,18 @@ const tasks = [
 function displaylist(){
   tasks.forEach(task =>{
     const li = document.createElement('li')
-    const text = `<div> ${task.description}</div>`
+    const text = `<div> <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+    ${task.description}</div>`
+    li.classList.add("list-group-item");
     li.innerHTML = text
     ul.appendChild(li)
+  
   })
+  const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Clear all Completed'
+    deleteButton.classList.add("list-group-item")
+    deleteButton.classList.add("block")
+    ul.appendChild(deleteButton)
 }
 
 displaylist();
