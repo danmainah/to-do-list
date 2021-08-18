76aa1a3908a769fdf1f32d00f1add23ea8ed46b7
unknown <danielmainawaweru@gmail.com>
2021-08-19 00:35:52 +0300

    Fix ESlint errors

--- src/status.js
+++ src/status.js
@@ -1,18 +1,20 @@
+/* eslint-disable comma-dangle, import/prefer-default-export, no-undef */
 import './style.css';
 import getTasks from './display.js';
 
 const list = document.querySelector('ul');
 
-export const checkbox = () => {
+export  const checkbox = () => {
   list.addEventListener(
     'click',
-    (checker = (ev) => {
+     (ev) => {
       if (ev.target.tagName === 'LI') {
         const task = ev.target.classList.toggle('checked');
         task.isCompleted = true;
         localStorage.setItem('task', JSON.stringify(getTasks));
       }
-    }),
+    },
     false
   );
 };
+/* eslint-enable comma-dangle, import/prefer-default-export, no-undef  */
